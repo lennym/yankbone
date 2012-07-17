38a382fcd7b3d3c0f2563efa2e2a178ddd5ed4ee
Lenny Martin <leonard.martin@gmail.com>
2012-07-17 15:50:52 +0100

    Moar xenophobia!!

--- yankbone.js
+++ yankbone.js
@@ -3,12 +3,12 @@
         _ = root._;
     if (!_ && (typeof require !== 'undefined')) _ = require('underscore');
     _.each(['View', 'Model', 'Collection', 'Router'], function (c) {
-        var _extend = Backbone[c].extend;
+        var ext = Backbone[c].extend;
         Backbone[c].extend = function (props) {
             if (props.hasOwnProperty('initialise')) {
-                throw new Error('Terribly sorry old chap, you seem to have misnamed your initialise method.');
+                throw new Error('Terribly sorry old chap, you seem to have named your initialize method correctly.');
             }
-            return _extend.apply(this, arguments);
+            return ext.apply(this, arguments);
         }
     });
 })(this);
